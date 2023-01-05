28ec17fe8d97dd229cbcd8f8e94ef039db98e5e9
hsnapps <prog.hasan@gmail.com>
2023-01-05 12:35:14 +0300

    First edit

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,5 @@
 public class HelloWorld {
-    public static void main(String[] args){
-        System.out.println("-----***** Hello World, Welcome to Jenkins Testing *****-----");
+    public static void main(String[] args) {
+        System.out.println("***** Hello World, Welcome to Jenkins Testing *****");
     }
 }
